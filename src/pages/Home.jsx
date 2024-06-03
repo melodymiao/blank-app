23368e275cf55bb296791b4cf44ae8dadf685b38
Melody Miao <melodymiao@berkeley.edu>
2024-06-03 01:00:11 -0700

    hello world

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import './Home.css'
+
+const Home = () => {
+    return (
+    <>
+    <div class="section-body">
+        <p>hello world</p>
+    </div>
+    </>
+);
+}
+  
+  export default Home;
